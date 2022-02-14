759ec9b62d7e6ebf0f3fc1f35f1b88d278bf2029
Tristan <LeoDog896@hotmail.com>
2022-02-13 20:24:01 -0500

    Fix tds

--- rollup-plugin-less.d.ts
+++ rollup-plugin-less.d.ts
@@ -1,17 +1,5 @@
 declare module "rollup-plugin-less" {
     
-    interface RollupPluginLessOptions {
-        insert?: boolean,
-        watch?: boolean,
-        option?: {
-            filename?: string
-        },
-        output?: string | boolean | (() => string),
-        include?: string | string[],
-        exclude?: string | string[]
-    }
-
-    import type { Plugin } from 'rollup';
-    function plugin(options: RollupPluginLessOptions): Plugin
-    export = plugin
+    function plugin(options: any): any
+    export default plugin
 }
