05066f16f6d44c7ff7b4b815e5e3c7782fe796cd
LeoDog896 <LeoDog896@hotmail.com>
2022-06-11 02:41:40 +0000

    Update dependencies & fix basic build

---
+++ script/build.ts
@@ -0,0 +1,129 @@
+import { rm, mkdir, readdir, copyFile, readFile, writeFile } from 'fs/promises';
+import less from 'less';
+import path from 'path';
+import { minify } from 'terser';
+import ts from 'typescript';
+
+async function copyDir(src: string, dest: string) {
+  await mkdir(dest, { recursive: true });
+  const entries = await readdir(src, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    entry.isDirectory()
+      ? await copyDir(srcPath, destPath)
+      : await copyFile(srcPath, destPath);
+  }
+}
+
+async function buildCSS() {
+  writeFile(
+    'build/mathquill.css',
+    (
+      await less.render(await readFile('src/css/main.less', 'utf-8'), {
+        paths: ['src/css'],
+      })
+    ).css,
+    'utf-8'
+  );
+}
+
+const baseSources = [
+  'src/utils.ts',
+  'src/dom.ts',
+  'src/unicode.ts',
+  'src/browser.ts',
+  'src/animate.ts',
+  'src/services/aria.ts',
+  'src/domFragment.ts',
+  'src/tree.ts',
+  'src/cursor.ts',
+  'src/controller.ts',
+  'src/publicapi.ts',
+  'src/services/parser.util.ts',
+  'src/services/saneKeyboardEvents.util.ts',
+  'src/services/exportText.ts',
+  'src/services/focusBlur.ts',
+  'src/services/keystroke.ts',
+  'src/services/latex.ts',
+  'src/services/mouse.ts',
+  'src/services/scrollHoriz.ts',
+  'src/services/textarea.ts',
+];
+
+const fullSources = [
+  ...baseSources,
+  'src/commands/math.ts',
+  'src/commands/text.ts',
+  'src/commands/math/advancedSymbols.ts',
+  'src/commands/math/basicSymbols.ts',
+  'src/commands/math/commands.ts',
+  'src/commands/math/LatexCommandInput.ts',
+];
+
+const basicSources = [
+  ...baseSources,
+  'src/commands/math.ts',
+  'src/commands/math/basicSymbols.ts',
+  'src/commands/math/commands.ts',
+];
+
+async function buildJS(
+  origin: string[],
+  out: string,
+  useIntroOutro: boolean,
+  shouldMinify: boolean
+) {
+  const sources: string[] = (
+    await Promise.all([
+      useIntroOutro ? await readFile('src/intro.js', 'utf8') : '',
+      ...origin.map(async (it) => {
+        if (it.endsWith('.ts')) {
+          return ts.transpileModule(await readFile(it, 'utf-8'), {
+            compilerOptions: { declarationDir: 'src' },
+          }).outputText;
+        } else {
+          return await readFile(it, 'utf-8');
+        }
+      }),
+      useIntroOutro ? await readFile('src/outro.js', 'utf8') : '',
+    ])
+  ).filter(Boolean);
+
+  if (shouldMinify) {
+    await writeFile(
+      `build/${out}.js`,
+      (
+        await minify(sources.join('\n'))
+      ).code,
+      'utf-8'
+    );
+  } else {
+    await writeFile(`build/${out}.js`, sources.join('\n'), 'utf-8');
+  }
+}
+
+(async () => {
+  await rm('build', { recursive: true, force: true });
+  await mkdir('build');
+  await copyDir('src/fonts', 'build/fonts');
+  await buildCSS();
+  await buildJS(fullSources, 'mathquill', true, false);
+  await buildJS(fullSources, 'mathquill.min', true, true);
+  await buildJS(basicSources, 'mathquill-basic', true, false);
+  await buildJS(basicSources, 'mathquill-basic.min', true, true);
+  await buildJS(
+    [
+      ...fullSources,
+      'test/support/assert.ts',
+      'test/support/trigger-event.ts',
+      'test/support/jquery-stub.ts',
+      ...[...(await readdir('test/unit'))].map((it) => 'test/unit/' + it),
+    ],
+    'mathquill.test',
+    true,
+    false
+  );
+})();
